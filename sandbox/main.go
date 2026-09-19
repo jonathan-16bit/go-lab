33a64bc355f8116bce421c5a3be112a441b62210
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-20 03:12:07 +0530

    feat: move variable-related code into variables_intro.go

--- sandbox/main.go
+++ sandbox/main.go
@@ -1,22 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"reflect"
+	// "fmt"
+	// "reflect"
 )
 
-// Package level
-var fact_2 = 2
-
 func main() {
-	// Function level
-	var fact_3 = 6
-
-	// Short variable declaration
-	fact_4 := 24
-
-	// Type is inferred from initializer
-	fmt.Println("Type of fact_2: ", reflect.TypeOf(fact_2))
-	fmt.Println("Type of fact_3: ", reflect.TypeOf(fact_3))
-	fmt.Println("Type of fact_4: ", reflect.TypeOf(fact_4))
+	//
 }
