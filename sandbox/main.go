89a61f35e605ccdbaa859cabb43d48459fe5d0be
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-20 03:17:06 +0530

    feat: add code to show uint64 and complex128 data types

--- sandbox/main.go
+++ sandbox/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	// "fmt"
-	// "reflect"
+	"fmt"
 )
 
 func main() {
-	//
+	var MaxInt uint64 = 1 << 64 - 1
+	var zComplex complex128 = 1i
+
+	fmt.Println("Max int: ", MaxInt)
+	fmt.Println("Square root of -1: ", zComplex)
 }
