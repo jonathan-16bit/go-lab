7174b9bc6d62887da60e4c4561ca20c6491b4195
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-20 02:35:13 +0530

    feat: add string swap function (multiple return values)

--- sandbox/main.go
+++ sandbox/main.go
@@ -9,6 +9,14 @@ func mult(a int, b int) int {
 	return a * b
 }
 
+// Multiple return values
+func swap(a, b string) (string, string) {
+	return b, a
+}
+
 func main() {
 	fmt.Println("Multiplication of 2 and 42 produces: ", mult(2, 42));
+
+	str1, str2 := swap("Sweet", "Sour")
+	fmt.Println(str1, str2)
 }
