f0ad3f579f22d691aa53d6b5faaf9acb475a5711
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-20 02:44:51 +0530

    feat: add inc_dec function with named return values

--- sandbox/main.go
+++ sandbox/main.go
@@ -14,9 +14,20 @@ func swap(a, b string) (string, string) {
 	return b, a
 }
 
+// Named return values
+func inc_dec(num int) (prev, next int) {
+	prev = num - 1
+	next = num + 1
+	// Naked return: returns named values
+	return
+}
+
 func main() {
 	fmt.Println("Multiplication of 2 and 42 produces: ", mult(2, 42));
 
 	str1, str2 := swap("Sweet", "Sour")
 	fmt.Println(str1, str2)
+
+	fmt.Println("Predecessor and successor of 43: ")
+	fmt.Println(inc_dec(43))
 }
