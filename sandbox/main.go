804bee00d83049b97fc44c39e9273eabfb39defe
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-20 17:07:54 +0530

    feat: add iterative while-loop style code for factorial fn

--- sandbox/main.go
+++ sandbox/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func factorial(n int) int {
+func factorial_for(n int) int {
 	fact := 1
 	for i := 1; i <= n; i++ {
 		fact *= i
@@ -13,7 +13,22 @@ func factorial(n int) int {
 	return fact
 }
 
+// Also for-loop, but while-style
+func factorial_while(n int) int {
+	fact, i := 1, 1
+
+	for i <= n {
+		fact *= i
+		i += 1
+	}
+
+	return fact
+}
+
 func main() {
 	n := 20
-	fmt.Printf("Factorial of %v: %v\n", n, factorial(n))
+	fmt.Printf("Factorial of %v: %v\n", n, factorial_for(n))
+
+	m := 20
+	fmt.Printf("Factorial of %v: %v\n", m, factorial_while(m))
 }
