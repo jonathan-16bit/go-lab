ed6f9ed048c0b39af7bc41d0149b7f8fc184301b
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-20 17:01:59 +0530

    feat: add iterative (for-loop) factorian fn

--- sandbox/main.go
+++ sandbox/main.go
@@ -4,5 +4,16 @@ import (
 	"fmt"
 )
 
+func factorial(n int) int {
+	fact := 1
+	for i := 1; i <= n; i++ {
+		fact *= i
+	}
+
+	return fact
+}
+
 func main() {
+	n := 20
+	fmt.Printf("Factorial of %v: %v\n", n, factorial(n))
 }
