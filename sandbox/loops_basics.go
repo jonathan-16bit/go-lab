c493bb5f6b0c1efeea44d9a34f82d5cc62cf71de
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-20 17:20:09 +0530

    feat: move loop factorial fns into loops_basics.go

---
+++ sandbox/loops_basics.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func factorial_for(n int) int {
+	fact := 1
+	for i := 1; i <= n; i++ {
+		fact *= i
+	}
+
+	return fact
+}
+
+// Also for-loop, but while-style
+func factorial_while(n int) int {
+	fact, i := 1, 1
+
+	for {
+		if i > n {
+			break
+		}
+
+		fact *= i
+		i += 1
+	}
+
+	return fact
+}
--- sandbox/main.go
+++ sandbox/main.go
@@ -1,38 +1,9 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 )
 
-func factorial_for(n int) int {
-	fact := 1
-	for i := 1; i <= n; i++ {
-		fact *= i
-	}
-
-	return fact
-}
-
-// Also for-loop, but while-style
-func factorial_while(n int) int {
-	fact, i := 1, 1
-
-	for {
-		if i > n {
-			break
-		}
-
-		fact *= i
-		i += 1
-	}
-
-	return fact
-}
-
 func main() {
-	n := 20
-	fmt.Printf("Factorial of %v: %v\n", n, factorial_for(n))
-
-	m := 20
-	fmt.Printf("Factorial of %v: %v\n", m, factorial_while(m))
+	//
 }
