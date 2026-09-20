4debf1d12438d75459edd7639e50fcde381a6f56
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-20 11:14:19 +0530

    feat: add code for type conversions (int, float64, uint)

--- sandbox/main.go
+++ sandbox/main.go
@@ -5,9 +5,14 @@ import (
 )
 
 func main() {
-	var MaxInt uint64 = 1 << 64 - 1
-	var zComplex complex128 = 1i
 
+	// Type conversions
+	var fl float64 = -1.2
+	var x int = int(fl)
+	var u uint64 = uint64(x)
+	fmt.Println(fl, x, u)
+
+	/*
 	// Zero values for non-initialized vars
 	var i int
 	var f float64
@@ -16,6 +21,10 @@ func main() {
 	// %v: default format, %q: in quotes
 	fmt.Printf("%v, %v, %v, %q\n", i, f, b, s)
 
+	// Types
+	var MaxInt uint64 = 1 << 64 - 1
+	var zComplex complex128 = 1i
 	fmt.Println("Max int: ", MaxInt)
 	fmt.Println("Square root of -1: ", zComplex)
+	*/
 }
