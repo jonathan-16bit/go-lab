4b7dcb282fc24b981ad1603e17c26a0141b750a9
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-20 11:07:43 +0530

    feat: add code for zero values and %v and %q format specifiers

--- sandbox/main.go
+++ sandbox/main.go
@@ -8,6 +8,14 @@ func main() {
 	var MaxInt uint64 = 1 << 64 - 1
 	var zComplex complex128 = 1i
 
+	// Zero values for non-initialized vars
+	var i int
+	var f float64
+	var b bool
+	var s string
+	// %v: default format, %q: in quotes
+	fmt.Printf("%v, %v, %v, %q\n", i, f, b, s)
+
 	fmt.Println("Max int: ", MaxInt)
 	fmt.Println("Square root of -1: ", zComplex)
 }
