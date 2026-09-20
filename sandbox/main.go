1b9fc6038f0c18770aef5361ba42028ae9b9b41d
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-20 22:15:17 +0530

    feat: add code for sign-based switch case factorial

--- sandbox/main.go
+++ sandbox/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	// "fmt"
+ "fmt"
+ "math"
 )
 
+func factorial(n int) int {
+	fact := 1
+	for i := 1; i <= n; i++ {
+		fact *= i
+	}
+
+	return fact
+}
+
 func main() {
-	//
+	num := 8
+	if num == 0 {
+		fmt.Println("Factorial of 0: 1")
+		return
+	}
+
+	sign := int(math.Abs(float64(num))) / num
+
+	switch sign {
+	case -1:
+		fmt.Println("Factorial undefined for negative numbers");
+	case 1:
+		fmt.Printf("Factorial of %v: %v\n", num, factorial(num))
+	default:
+		fmt.Println("This should NOT print")
+	}
 }
