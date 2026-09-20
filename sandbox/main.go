dc56d527fa0aa991b1462308690700608fef88bb
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-20 22:28:42 +0530

    feat: add basic defer demo

--- sandbox/main.go
+++ sandbox/main.go
@@ -4,23 +4,7 @@ import (
  "fmt"
 )
 
-func factorial(n int) int {
-	fact := 1
-	for i := 1; i <= n; i++ {
-		fact *= i
-	}
-
-	return fact
-}
-
 func main() {
-	num := 8
-	switch true {
-	case num < 0:
-		fmt.Println("Factorial undefined for negative numbers");
-	case true:
-		fmt.Printf("Factorial of %v: %v\n", num, factorial(num))
-	default:
-		fmt.Println("This should NOT print")
-	}
+	defer fmt.Println("went up the hill")
+	fmt.Printf("Jack and Jill ")
 }
