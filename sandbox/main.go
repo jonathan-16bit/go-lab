7199d6a60246a7b4ef669ba73dbfdf1326b89a40
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-21 17:26:26 +0530

    feat: add range form of for-loop to print powers of 2

--- sandbox/main.go
+++ sandbox/main.go
@@ -5,14 +5,10 @@ import (
 )
 
 func main() {
-	// Creating slices with make()
-	// len=1, cap=10
-	numSlice := make([]int, 0, 10)
-	fmt.Println("Before append:", numSlice)
+	pow := []int{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024}
 
-	for i := 0; i <= 9; i++ {
-		numSlice = append(numSlice, i)
+	// range form: iterate over indices and values
+	for i, v := range pow {
+		fmt.Printf("2**%v = %v\n", i, v)
 	}
-
-	fmt.Println("After append:", numSlice)
 }
--- sandbox/slice_basics.go
+++ sandbox/slice_basics.go
@@ -57,3 +57,16 @@ func slice_basics() {
 		fmt.Println("nil slice")
 	}
 }
+
+func appendSlice() {
+	// Creating slices with make()
+	// len=1, cap=10
+	numSlice := make([]int, 0, 10)
+	fmt.Println("Before append:", numSlice)
+
+	for i := 0; i <= 9; i++ {
+		numSlice = append(numSlice, i)
+	}
+
+	fmt.Println("After append:", numSlice)
+}
