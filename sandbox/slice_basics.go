32e4a64f04c2ef8e45826dd13e85379cdbd31768
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-21 17:54:59 +0530

    feat: add fn to demo range form of for-loops

--- sandbox/slice_basics.go
+++ sandbox/slice_basics.go
@@ -70,3 +70,12 @@ func appendSlice() {
 
 	fmt.Println("After append:", numSlice)
 }
+
+func rangeForm() {
+	pow := []int{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024}
+
+	// range form: iterate over indices and values
+	for i, v := range pow {
+		fmt.Printf("2**%v = %v\n", i, v)
+	}
+}
