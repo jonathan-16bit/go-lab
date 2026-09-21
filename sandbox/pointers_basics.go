4c5098019564173fe1d5da81b5481807caaecc71
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-21 10:17:10 +0530

    feat: move pointers code into pointers_basics.go

---
+++ sandbox/pointers_basics.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func pointers_demo() {
+	a, b := 42, 0
+
+	var p1 *int = &a
+	fmt.Printf("Var a (value: %v) at address: %p\n", a, p1)
+
+	b = *p1
+	fmt.Printf("Var b after copying from dereferenced pointer to a: %v\n", b)
+}
