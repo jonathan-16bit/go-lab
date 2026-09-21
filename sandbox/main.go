8d9ad0b63cf8d241eb647a7667443fe035cb5882
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-21 20:32:14 +0530

    feat: move makeAdderN and Fibonacci closure code into closures.go

--- sandbox/main.go
+++ sandbox/main.go
@@ -4,34 +4,6 @@ import (
  "fmt"
 )
 
-func makeAdderN(n int) func(int) int {
-	return func(k int) int {
-		return n + k
-	}
-}
-
-func fibonacci() func() int {
-	a, b := 0, 1
-	return func() int {
-		next := a
-		// RHS is evaluated using the old a and b
-		a, b = b, a + b
-		return next
-	}
-}
-
 func main() {
-	/*
-	// Closures
-	add10 := makeAdderN(10)
-	for i := 0; i <= 10; i++ {
-		fmt.Printf("Addition of 10 with %v = %v\n", i, add10(i))
-	}
-	*/
-
-	// Fibonacci
-	f := fibonacci()
-	for i := 0; i <= 10; i++ {
-		fmt.Println(f())
-	}
+	//
 }
