3c7e31406d072c9ca0e0a3f9c765ae982af1e9ec
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-21 07:10:57 +0530

    feat: add basic pointer code

--- sandbox/main.go
+++ sandbox/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
- // "fmt"
+ "fmt"
 )
 
 func main() {
+	a, b := 42, 0
+
+	var p1 *int = &a
+	fmt.Printf("Var a (value: %v) at address: %p\n", a, p1)
+
+	b = *p1
+	fmt.Printf("Var b after copying from dereferenced pointer to a: %v\n", b)
 }
