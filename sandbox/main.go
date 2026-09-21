0acce0855dea069671d1ac2adac042216c85d84d
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-21 20:30:53 +0530

    feat: add Fibonacci closure code

--- sandbox/main.go
+++ sandbox/main.go
@@ -10,10 +10,28 @@ func makeAdderN(n int) func(int) int {
 	}
 }
 
+func fibonacci() func() int {
+	a, b := 0, 1
+	return func() int {
+		next := a
+		// RHS is evaluated using the old a and b
+		a, b = b, a + b
+		return next
+	}
+}
+
 func main() {
+	/*
 	// Closures
 	add10 := makeAdderN(10)
 	for i := 0; i <= 10; i++ {
 		fmt.Printf("Addition of 10 with %v = %v\n", i, add10(i))
+	}
+	*/
+
+	// Fibonacci
+	f := fibonacci()
+	for i := 0; i <= 10; i++ {
+		fmt.Println(f())
 	}
 }
