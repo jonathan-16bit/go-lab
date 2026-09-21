b85bdb32f6245dec6bf8b087e4280125a64321c0
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-21 10:14:48 +0530

    feat: add code for structs and struct pointers

--- sandbox/main.go
+++ sandbox/main.go
@@ -4,7 +4,23 @@ import (
  "fmt"
 )
 
+type Vertex struct {
+	X int
+	Y int
+}
+
 func main() {
+	// Access struct fields with dot
+	v := Vertex{4, 2}
+
+	// Struct pointer
+	p := &v 
+
+	// We can use without explicit dereference: p.X instead of (*p).X
+	fmt.Println("The x coordinate: ", p.X)
+	fmt.Println("The y coordinate: ", p.Y)
+
+	/*
 	a, b := 42, 0
 
 	var p1 *int = &a
@@ -12,4 +28,5 @@ func main() {
 
 	b = *p1
 	fmt.Printf("Var b after copying from dereferenced pointer to a: %v\n", b)
+	*/
 }
