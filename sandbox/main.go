973728e2e040e6bd70a68d3e9da939c32a2c535c
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-21 20:14:16 +0530

    feat: add closure code with makeAdderN

--- sandbox/main.go
+++ sandbox/main.go
@@ -4,6 +4,16 @@ import (
  "fmt"
 )
 
+func makeAdderN(n int) func(int) int {
+	return func(k int) int {
+		return n + k
+	}
+}
+
 func main() {
-	//
+	// Closures
+	add10 := makeAdderN(10)
+	for i := 0; i <= 10; i++ {
+		fmt.Printf("Addition of 10 with %v = %v\n", i, add10(i))
+	}
 }
