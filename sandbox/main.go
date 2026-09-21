b65af42dfe1f58c5f9237d7f9d47d9d92c5a8dec
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-21 17:19:39 +0530

    feat: add slice creation and append code with digit for-loop

--- sandbox/main.go
+++ sandbox/main.go
@@ -5,5 +5,14 @@ import (
 )
 
 func main() {
-	//
+	// Creating slices with make()
+	// len=1, cap=10
+	numSlice := make([]int, 0, 10)
+	fmt.Println("Before append:", numSlice)
+
+	for i := 0; i <= 9; i++ {
+		numSlice = append(numSlice, i)
+	}
+
+	fmt.Println("After append:", numSlice)
 }
