dd841a612dd796334f223f9af5c3207391bde721
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-21 10:21:18 +0530

    feat: add code for struct literals

--- sandbox/main.go
+++ sandbox/main.go
@@ -5,10 +5,18 @@ import (
 )
 
 type Vertex struct {
-	X int
-	Y int
+	X, Y int
 }
 
 func main() {
-	//
+	// Order of named fields is irrelevant
+	v1 := Vertex{4, 2}
+	v2 := Vertex{Y: 3, X: 4}
+
+	v4 := Vertex{Y: 1}  // X: 0
+	v3 := Vertex{}  // Both are 0
+
+	v5 := &Vertex{4, 4}
+
+	fmt.Println(v1, v2, v3, v4, v5)
 }
