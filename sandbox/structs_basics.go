fe3d025f122a89972cb79751f7a02ab49b845cdb
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-21 10:22:04 +0530

    feat: move structs code into structs_basics.go

---
+++ sandbox/structs_basics.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+ "fmt"
+)
+
+type Vertex struct {
+	X, Y int
+}
+
+func structs_basics() {
+	// Order of named fields is irrelevant
+	v1 := Vertex{4, 2}
+	v2 := Vertex{Y: 3, X: 4}
+
+	v4 := Vertex{Y: 1}  // X: 0
+	v3 := Vertex{}  // Both are 0
+
+	v5 := &Vertex{4, 4}
+
+	fmt.Println(v1, v2, v3, v4, v5)
+}
